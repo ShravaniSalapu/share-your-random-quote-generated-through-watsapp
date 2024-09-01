a5afa2675b1f1dcff6e2cb75870fdfa34fe59524
ShravaniSalapu <130199411+ShravaniSalapu@users.noreply.github.com>
2024-09-01 20:50:07 +0530

    Add files via upload

---
+++ quote/script.js
@@ -0,0 +1,26 @@
+const quote=document.querySelector("#quote");
+const author=document.querySelector("#author");
+const but=document.querySelector("#but");
+const twt=document.querySelector("#twt");
+const reset=document.querySelector("#reset");
+but.addEventListener("click",getQuote);
+function getQuote(){
+    fetch("https://quotable.io/random")
+    .then(res=>res.json())
+    .then(data=>{
+        quote.textContent=data.content;
+        author.textContent=data.author;
+    }
+
+    )
+}
+twt.addEventListener("click",tweet);
+function tweet(){
+    window.open("https://wa.me/?text="+quote.textContent+"--by " + author.textContent);
+}
+reset.addEventListener("click",re);
+function re(){
+    quote.textContent="Quote here by...";
+    author.textContent="author";
+
+}
